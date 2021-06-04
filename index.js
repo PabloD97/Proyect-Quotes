dcb1c32d6afadf99082f1a44fef1bff70b469880
PabloD97 <pablo97758@gmail.com>
2021-06-03 22:59:20 -0300

    feat: Server
    - Se agrego el middleware de error para cuando se ingrese a una ruta erronea
    - Se agrego el middleware express.json() para transformar las peticiones post en formato JSON
    - Se agregaron las redirecciones para HomeRoutes y QuotesRoutes
    - Se indico la ruta publica que seria /public

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const server = express();
 const {PORT} = require('./config');
+const {HomeRoutes, QuotesRoutes} = require('./routes');
+const {NotFoundMiddleware} = require('./middleware');
+
+server.use(express.static('./public'));
+server.use(express.json());
+
+server.use('/', HomeRoutes);
+server.use('/', QuotesRoutes);
+server.use(NotFoundMiddleware);
 
 server.listen(PORT, ()=> {
     console.log(`Aplication running in port ${PORT}`)
-})
+}); 
+
